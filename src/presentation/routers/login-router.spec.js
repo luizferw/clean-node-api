d8d546a8cae62f8a634cfe6b3c6323ad96fb19c8
Luiz Fernando Witt <luizfernandowitt00@gmail.com>
2022-09-23 13:58:27 -0300

    ensure LoginRouter returns 400 if no email is provided

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,26 @@
+class LoginRouter {
+  route (httpRequest) {
+    const { email } = httpRequest.body
+
+    if (!email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  it('should returns 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
